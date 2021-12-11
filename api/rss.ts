5d2e449a9df4d465d55ac61520c9c15122d2a7e8
hdewey <hdewey02@gmail.com>
2021-12-10 16:16:38 -0800

    nope

--- api/rss.ts
+++ api/rss.ts
@@ -31,8 +31,8 @@ const fuse = initFuseWithProviders(alchemyURL);
 const web3 = new Web3(alchemyURL);
 
 // url for making requests to the rss-module apis (requests are api's instead of modules so vercel can cache each asset for scoring subsequent pools)
-// const url = "https://rari-rss.vercel.app/api";
-const url = "http://localhost:3000/api";
+const url = "https://rari-rss.vercel.app/api";
+// const url = "http://localhost:3000/api";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (request: VercelRequest, response: VercelResponse) => {
